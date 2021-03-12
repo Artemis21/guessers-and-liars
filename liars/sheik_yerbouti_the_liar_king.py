b74b866e86dad90c8197e91c586b8f29f0defd7d
SheikYerbouty <76229672+SheikYerbouty@users.noreply.github.com>
2021-03-12 13:09:11 +0000

    Create sheik_yerbouti_the_liar_king.py

---
+++ liars/sheik_yerbouti_the_liar_king.py
@@ -0,0 +1,24 @@
+# Liar: theLiarKing
+
+import random, math
+
+class Main:
+
+	def __init__(self, rng: random.Random, secret: int):
+		self.rng = rng
+		self.secret = secret
+
+	def __call__(self, guess: int) -> int:
+		timeToLie = self.rng.randint(1,16)
+		if guess == self.secret:
+			res = self.rng.choice([-1,1])
+		elif timeToLie == 1:
+			if self.secret > guess:
+				res = -1
+			else:
+				res = 1
+		elif self.secret > guess:
+			res = 1
+		else:
+			res = -1
+		return res
